c3c550a65cb1701ead96704a84f987a94513e518
Yury Popov <git@phoenix.dj>
2022-10-31 11:48:38 +0300

    Add startup screen tracking initialization progress Related to #4

--- Application/EspeakNgApp.swift
+++ Application/EspeakNgApp.swift
@@ -6,40 +6,24 @@
 //
 
 import SwiftUI
-import AVFAudio
 
 @main
 struct EspeakNgApp: App {
-  init() {
-    do {
-      try setupSynth()
-    } catch let e {
-      print(e)
-    }
-
-    #if os(iOS)
-    let (langs, voices) = _splitVoices(_buildVoiceList())
-    let mapping = _buildMappings(langs)
-    let groupData = UserDefaults(suiteName: "group.dj.phoenix.espeak-ng")
-    do {
-      let enc = JSONEncoder()
-      groupData?.synchronize()
-      groupData?.set(try? enc.encode(langs), forKey: "langs")
-      groupData?.set(try? enc.encode(voices), forKey: "voices")
-      groupData?.set(try? enc.encode(mapping), forKey: "mapping")
-      groupData?.synchronize()
-    }
-
-    AVSpeechSynthesisProviderVoice.updateSpeechVoices()
-    #endif
-  }
+  @State var espeakState: EspeakState = .setup
   var body: some Scene {
     WindowGroup {
-      #if os(iOS)
-      NavigationView { ContentView() }
-      #else
-      ContentView()
-      #endif
+      if case .done = espeakState {
+        #if os(iOS)
+        NavigationView { ContentView() }
+        #else
+        ContentView()
+        #endif
+      } else {
+        InitScreen(state: $espeakState)
+        #if !os(iOS)
+          .frame(minWidth: 640, minHeight: 480)
+        #endif
+      }
     }
   }
 }
---
+++ Application/InitScreen.swift
@@ -0,0 +1,67 @@
+//
+//  InitScreen.swift
+//  EspeakNg
+//
+//  Created by Yury Popov on 31.10.2022.
+//
+
+import SwiftUI
+import AVFAudio
+
+enum EspeakState {
+  case setup
+  case extracted
+  case done
+  case error(Error)
+}
+
+struct InitScreen: View {
+  @Binding var state: EspeakState
+  var body: some View {
+    VStack {
+      switch state {
+      case .setup:
+        ProgressView()
+        Text("Starting espeak-ng engine...")
+          .onAppear {
+            do {
+              try setupSynth()
+              withAnimation { self.state = .extracted }
+            } catch let e {
+              withAnimation { self.state = .error(e) }
+            }
+          }
+      case .extracted:
+  #if os(iOS)
+        ProgressView()
+        Text("Syncing VoiceOver voices...")
+          .onAppear {
+            let (langs, voices) = _splitVoices(_buildVoiceList())
+            let mapping = _buildMappings(langs)
+            let groupName = "group.\(Bundle.app.bundleIdentifier!)"
+            let groupData = UserDefaults(suiteName: groupName)
+            do {
+              let enc = JSONEncoder()
+              groupData?.synchronize()
+              groupData?.set(try enc.encode(langs), forKey: "langs")
+              groupData?.set(try enc.encode(voices), forKey: "voices")
+              groupData?.set(try enc.encode(mapping), forKey: "mapping")
+              groupData?.synchronize()
+
+              AVSpeechSynthesisProviderVoice.updateSpeechVoices()
+
+              withAnimation { self.state = .done }
+            } catch let e {
+              withAnimation { self.state = .error(e) }
+            }
+          }
+  #else
+        Text(" ").onAppear { withAnimation { self.state = .done } }
+  #endif
+      case .done: Text("Done")
+      case .error(let e): Text("eSpeak failure: \(String(describing: e))")
+      }
+    }.padding().background(RoundedRectangle(cornerRadius: 8).foregroundColor(.secondaryBackground))
+  }
+}
+
